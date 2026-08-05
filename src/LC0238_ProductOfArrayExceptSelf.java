817a96affda862e1b54765ffae3a90879d536dc5
Daoxian <daoxian@macmini.local>
2026-08-06 05:01:43 +0800

    LC238 空间优化：答案数组存前缀积+滚动后缀，额外空间 O(1)，二次提交 Accepted，复盘更新为最优

--- src/LC0238_ProductOfArrayExceptSelf.java
+++ src/LC0238_ProductOfArrayExceptSelf.java
@@ -23,15 +23,12 @@ public class LC0238_ProductOfArrayExceptSelf {
         for (int i = 1; i < leftMul.length; i++) {
             leftMul[i] = leftMul[i - 1] * nums[i - 1];
         }
-        int[] rightMul = new int[nums.length];
-        rightMul[nums.length - 1] = 1;
-        for (int i = nums.length - 2; i >= 0; i--) {
-            rightMul[i] = rightMul[i + 1] * nums[i + 1];
+        int right = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            leftMul[i] = leftMul[i] * right;
+            right *= nums[i];
         }
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = leftMul[i] * rightMul[i];
-        }
-        return nums;
+        return leftMul;
     }
     // ==== 提交代码结束 ====
 
